be5bde3f0a555b880a7e4f6c54643502fe88f9dc
Voki "Pro" Toki <45886099+Fridgie@users.noreply.github.com>
2021-04-14 22:29:38 +0200

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,23 @@
+music.play_tone(262, music.beat(BeatFraction.WHOLE))
+music.play_tone(262, music.beat(BeatFraction.WHOLE))
+music.play_tone(294, music.beat(BeatFraction.WHOLE))
+music.play_tone(262, music.beat(BeatFraction.WHOLE))
+music.play_tone(349, music.beat(BeatFraction.WHOLE))
+music.play_tone(330, music.beat(BeatFraction.WHOLE))
+music.play_tone(262, music.beat(BeatFraction.WHOLE))
+music.play_tone(262, music.beat(BeatFraction.WHOLE))
+music.play_tone(294, music.beat(BeatFraction.WHOLE))
+music.play_tone(262, music.beat(BeatFraction.WHOLE))
+music.play_tone(392, music.beat(BeatFraction.WHOLE))
+music.play_tone(349, music.beat(BeatFraction.WHOLE))
+music.play_tone(262, music.beat(BeatFraction.WHOLE))
+music.play_tone(262, music.beat(BeatFraction.WHOLE))
+music.play_tone(262, music.beat(BeatFraction.WHOLE))
+music.play_tone(440, music.beat(BeatFraction.WHOLE))
+music.play_tone(349, music.beat(BeatFraction.WHOLE))
+music.play_tone(330, music.beat(BeatFraction.WHOLE))
+music.play_tone(294, music.beat(BeatFraction.WHOLE))
+
+def on_forever():
+    pass
+basic.forever(on_forever)
